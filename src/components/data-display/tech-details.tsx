1f660eadb64b51df468d21d85abc6b01a8577350
Prince <princekumar.priku@gmail.com>
2024-03-12 19:40:37 +0530

    fixing issue

--- src/components/data-display/tech-details.tsx
+++ src/components/data-display/tech-details.tsx
@@ -4,11 +4,11 @@ import { TechDetails } from '@/lib/types';
 import Typography from '@/components/general/typography';
 import spaceGrotesk from '../general/space-grotesk-font';
 
-const TechDetails = ({ label }: TechDetails) => {
+const TechDetail = ({ label }: TechDetails) => {
   return (
     <div className="flex flex-col items-center gap-2">
-      
-        {/* <Link noCustomization href={url} externalLink>
+
+      {/* <Link noCustomization href={url} externalLink>
           <ImageWrapper
             src={logo}
             srcForDarkMode={darkModeLogo}
@@ -16,13 +16,13 @@ const TechDetails = ({ label }: TechDetails) => {
             className="transition-transform duration-300 md:hover:scale-110"
           />
         </Link> */}
-     
+
       <div className='pt-3 pb-3 pl-7 pr-7 rounded-lg border-solid border-2 border-grey-700'>
-      <Typography className={`${spaceGrotesk.className} text-xs color-black-900`} variant="body1">{label}</Typography>
+        <Typography className={`${spaceGrotesk.className} text-xs color-black-900`} variant="body1">{label}</Typography>
       </div>
 
     </div>
   );
 };
 
-export default TechDetails;
+export default TechDetail;
--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -3,7 +3,7 @@ import Tag from '@/components/data-display/tag';
 import Container from '@/components/layout/container';
 import ScrollAnimation from '@/hooks/scrollAnimation';
 import spaceGrotesk from '../general/space-grotesk-font';
-import TechDetails from '../data-display/tech-details';
+import TechDetail from '../data-display/tech-details';
 
 const SkillsSection = () => {
   return (
@@ -14,19 +14,12 @@ const SkillsSection = () => {
             <Tag className={`${spaceGrotesk.className} text-lg`} label="Core Skills" />
           </div>
         </ScrollAnimation>
-        {/* <ScrollAnimation>
-          <Reavel>
-            <Typography variant="subtitle" className="max-w-xl text-center">
-            I&apos;ve explored various tools and technologies throughout my journey
-            </Typography>
-          </Reavel>
-        </ScrollAnimation> */}
       </div>
 
       {/* grid grid-cols-3 gap-y-4 md:grid-cols-6 md:gap-y-8 lg:grid-cols-8 lg:gap-y-12 */}
       <div className="flex gap-6 flex-wrap justify-center">
         {TECHNOLOGIES.map((technology, index) => (
-          <TechDetails {...technology} key={index} />
+          <TechDetail {...technology} key={index} />
         ))}
       </div>
 
