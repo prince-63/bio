4151df166a3298ec381907163721d107fdebc068
Prince K. P <111671729+prince-63@users.noreply.github.com>
2024-04-06 20:56:15 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <HeroSection />
       <AboutMeSection />
       <SkillsSection />
-{/*       <ProjectSection /> */}
+      <ProjectSection /> 
       <ContactSection />
     </>
   );
