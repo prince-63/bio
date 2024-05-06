b6d760c76b38583eea28508d0430f56aa1bdd0c4
Prince K. P. <111671729+prince-63@users.noreply.github.com>
2024-05-06 02:55:17 +0000

    .

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,8 +4,11 @@ import Button from '@/components/general/button';
 
 const DownloadCV = () => {
   return (
-    <Button onClick={() => window?.open('https://github.com/prince-63/bio/tree/main/public/files/Resume.pdf', '_blank')}>
-      Get Resume
+    // <Button onClick={() => window?.open('https://github.com/prince-63/bio/tree/main/public/files/Resume.pdf', '_blank')}>
+    //   Get Resume
+    // </Button>
+    <Button>
+      <a href="https://prince-63.github.io/bio/files/Resume.pdf" download>Get Resume</a>
     </Button>
   );
 };
