630885d5a7fc9b9d76e508ed280236ae3b75cbfd
Prince Kushwaha <princekumar.priku@gmail.com>
2024-05-23 10:04:55 +0530

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -21,7 +21,7 @@ import ThemeSwitcher from '../general/theme-switcher';
 
 const Logo = () => (
   <Typography variant="h3" className=" font-semibold">
-    {'<🇮🇳/>'}
+    {'<PK/>'}
   </Typography>
 );
 
